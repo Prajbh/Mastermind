3bce55eba81483773f8501042f5cd22b6919abb8
Richard Zhang <rzhang3@umbc.edu>
2021-05-09 19:28:20 -0400

    Added more questions

--- app/src/main/java/com/example/mastermind/LogInFragment.java
+++ app/src/main/java/com/example/mastermind/LogInFragment.java
@@ -1,22 +1,36 @@
 package com.example.mastermind;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.view.menu.ActionMenuItemView;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LogInFragment extends Fragment {
 
     NavController navController;
-
-
+    private FirebaseAuth mauth;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference dbusers = database.getReference();
+    private EditText username,password;
     public LogInFragment() {
 
         // Required empty public constructor
@@ -26,7 +40,6 @@ public class LogInFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
     }
 
@@ -55,4 +68,5 @@ public class LogInFragment extends Fragment {
             }
         });
     }
+
 }
